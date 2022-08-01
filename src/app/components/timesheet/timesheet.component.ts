d4952a7b8aa4903e4243fc7fb7a534a230317488
tbergeson <talmage.bergeson3761@stu.mtec.edu>
2022-08-01 12:42:50 -0600

    completed unit #8

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -16,6 +16,7 @@ export class TimesheetComponent implements OnInit {
   employeeNameFC = new FormControl('', this.nameValidator());
   employees: Employee[] = [];
   employeeId = 0;
+  weekdays: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
   constructor(
     private route: ActivatedRoute,
@@ -35,7 +36,14 @@ export class TimesheetComponent implements OnInit {
         id: this.employeeId.toString(),
         departmentId: this.department?.id,
         name: this.employeeNameFC.value,
-        payRate: Math.floor(Math.random()*50) + 50
+        payRate: Math.floor(Math.random() * 50) + 50,
+        monday: 0,
+        tuesday: 0,
+        wednesday: 0,
+        thursday: 0,
+        friday: 0,
+        saturday: 0,
+        sunday: 0
       })
 
       this.employeeNameFC.setValue('');
@@ -43,17 +51,26 @@ export class TimesheetComponent implements OnInit {
   }
 
   nameValidator(): ValidatorFn {
-    return (control: AbstractControl): { [key: string]: any} | null => {
+    return (control: AbstractControl): { [key: string]: any } | null => {
       let error = null;
       if (this.employees && this.employees.length) {
         this.employees.forEach(employee => {
           if (employee.name.toLocaleLowerCase() === control.value.toLocaleLowerCase()) {
-            error = {duplicate: true};
+            error = { duplicate: true };
           }
         })
       }
       return error;
     }
+  }
+
+  getTotalHours(employee: Employee): number {
+    return employee.monday + employee.tuesday + employee.wednesday + employee.thursday +
+      employee.friday + employee.saturday + employee.sunday;
+  }
+
+  deleteEmployee(index: number): void {
+    this.employees.splice(index, 1);
   }
 
 }
