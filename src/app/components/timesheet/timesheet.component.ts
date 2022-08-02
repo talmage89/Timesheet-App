0e8da44c0c704b4ccbe07beeff8a1820e096e2ed
tbergeson <talmage.bergeson3761@stu.mtec.edu>
2022-08-02 07:37:41 -0600

    completed unit #13

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, ValidatorFn } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Department } from 'src/app/interfaces/department';
 import { Employee } from 'src/app/interfaces/employee';
 import { DepartmentsService } from 'src/app/services/departments.service';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-timesheet',
@@ -11,7 +13,7 @@ import { DepartmentsService } from 'src/app/services/departments.service';
   styleUrls: ['./timesheet.component.scss']
 })
 export class TimesheetComponent implements OnInit {
-  departments: Department[] | undefined;
+  $departments: Observable<Department[]> | undefined;
   department: Department | undefined;
   employeeNameFC = new FormControl('', this.nameValidator());
   employees: Employee[] = [];
@@ -20,12 +22,17 @@ export class TimesheetComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private departmentsService: DepartmentsService
+    private router: Router,
+    private departmentsService: DepartmentsService,
+    private employeeService: EmployeeService
   ) { }
 
   ngOnInit(): void {
-    this.departments = this.departmentsService.departments;
-    this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+    this.$departments = this.departmentsService.getDepartments();
+
+    this.$departments.subscribe(x => {
+      this.department = x.find(department => department.id === this.route.snapshot.params['id']);
+    })
   }
 
   addEmployee(): void {
@@ -33,7 +40,6 @@ export class TimesheetComponent implements OnInit {
       this.employeeId++;
 
       this.employees.push({
-        id: this.employeeId.toString(),
         departmentId: this.department?.id,
         name: this.employeeNameFC.value,
         payRate: Math.floor(Math.random() * 50) + 50,
@@ -71,6 +77,14 @@ export class TimesheetComponent implements OnInit {
 
   deleteEmployee(index: number): void {
     this.employees.splice(index, 1);
+  }
+
+  submit(): void {
+    this.employees.forEach(employee => {
+      this.employeeService.saveEmployeeHours(employee);
+    })
+
+    this.router.navigate(['/departments']);
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAlZZBGbAdDFYCC1vztpQT28oZoMt4tLq4",
+    authDomain: "timesheet-app-83f91.firebaseapp.com",
+    projectId: "timesheet-app-83f91",
+    storageBucket: "timesheet-app-83f91.appspot.com",
+    messagingSenderId: "937290181154",
+    appId: "1:937290181154:web:a81b85cf31697471bef87b"
+  }
 };
 
 /*
