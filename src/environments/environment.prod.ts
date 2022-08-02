40911dbb9bf9fb34b5faf71748d34985df4f69f5
tbergeson <talmage.bergeson3761@stu.mtec.edu>
2022-08-02 09:19:59 -0600

    added firebase hosting and automatic builds and deploys through github.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAlZZBGbAdDFYCC1vztpQT28oZoMt4tLq4",
+    authDomain: "timesheet-app-83f91.firebaseapp.com",
+    projectId: "timesheet-app-83f91",
+    storageBucket: "timesheet-app-83f91.appspot.com",
+    messagingSenderId: "937290181154",
+    appId: "1:937290181154:web:a81b85cf31697471bef87b"
+  }
 };
